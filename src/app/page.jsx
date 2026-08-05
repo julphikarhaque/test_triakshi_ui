34d0076874b1f5a9a7eb095719b76416da3e6287
julphikar haque <julphikarhoque880@gmail.com>
2026-08-05 17:18:21 +0530

    emailinput

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,28 @@
-import Image from "next/image";
 import InputWithLabel from "@/components/fromElements/inputWithLabel";
+import EmailInput from "@/components/fromElements/EmailInput";
+
 
 export default function Home() {
   return (
     <div className=" h-screen w-full bg-zinc-50 font-sans dark:bg-black flex justify-center items-center ">
-      <InputWithLabel name={"name"} inputClassName="" />
+      {/* <InputWithLabel name={"name"} inputClassName="" />
       <InputWithLabel
         name={"email"}
         inputClassName=""
         inputProps={{
           type: "email",
         }}
-      />
+      /> */}
+
+      <EmailInput
+          label="Email Address"
+          helperText="We'll never share your email."
+
+          inputProps={{
+            placeholder: "Enter your email",
+            required: true,
+          }}
+        />
     </div>
   );
 }
